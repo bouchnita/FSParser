5334bf28778da39af4213adff1b21e3fca36f9eb
Ilyass Elannid <ielannid@gmail.com>
2023-03-29 18:09:44 +0000

    same as last

---
+++ readfs.py
@@ -0,0 +1,44 @@
+#ntfs parsing functions
+
+
+import binascii
+import mbrparse as m
+#this function converts little endian to big endian, creds to dkuers on stackoverflow
+def lil2BigEndian(hex):
+    ba = bytearray.fromhex(hex.decode("utf-8"))
+    ba.reverse()
+    s = ''.join(format(x, '02x') for x in ba)
+    s = bytes(s, 'utf-8')
+    return s
+
+#function to calculate the fs starting address
+def calStartAddr(addr):
+    A=str(hex(int.from_bytes(binascii.unhexlify(lil2BigEndian(addr)))*512+1024)).replace('0x', '')
+    return A
+
+#function to calculate the fs ending address
+def calEndAddr(startAddr, numOfSec):
+    B=str(hex(int.from_bytes(binascii.unhexlify(lil2BigEndian(numOfSec)))*512+int.from_bytes(binascii.unhexlify(lil2BigEndian(startAddr)))*512+1024)).replace('0x', '')
+    return B
+
+LBA=b'3f000000'
+numSectors=b'8a340200'
+
+fsContent=m.readFroma2b(calStartAddr(LBA), calEndAddr(LBA, numSectors))
+
+
+
+
+
+# print(NTFS)
+# print(calEndAddr(LBA, tb))
+# print(type(calStartAddr(LBA)))
+# print(int('F',base=16))
+# print(str(int.from_bytes(binascii.unhexlify(ta))))
+# print(type(ta))
+# print(hex(int(decode(ta))+int(decode(tb))))
+# string='5a223f'
+# arr = bytes(string, 'utf-8')
+# print(type(arr))
+
+
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,5 +1,2 @@
-dFroma2b('8200','8202'))
-# print(readFroma2b('01BE','8202'))
-# print(readFroma2b('01CE','8202'))
-# print(readFroma2b('01DE','8202'))
-# print(rea
+ddr(LBA, tb))
+# print(calStar
